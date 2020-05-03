b856e25f6f78044322c01249c3b6c700a8abf9ef
Kevin Le <kevin_le@sfu.ca>
2020-05-02 17:05:12 -0700

    Add count up animation

--- src/components/GlobalCards/index.js
+++ src/components/GlobalCards/index.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom'
 import { Statistic, Divider, Header,
          Icon, Grid }
 from 'semantic-ui-react'
+import CountUp from 'react-countup';
+
 
 class GlobalCards extends Component {
 
@@ -25,16 +27,60 @@ class GlobalCards extends Component {
    
     return (
       <div className={this.props.className}>
+        
         <Statistic color='blue' className='statSubBox'>
-          <Statistic.Value>{this.numberWithCommas(this.props.confirmed)}</Statistic.Value>
+          <Statistic.Value>
+            <CountUp
+              start={0}
+              end={this.props.confirmed}
+              duration={1}
+              separator=','
+              delay={0}
+            >
+              {({ countUpRef }) => (
+                <div>
+                  <span ref={countUpRef} />
+                </div>
+              )}
+            </CountUp>
+          </Statistic.Value>
           <Statistic.Label>Confirmed Cases</Statistic.Label>
         </Statistic>
         <Statistic color='red' className='statSubBox'>
-          <Statistic.Value>{this.numberWithCommas(this.props.deaths)}</Statistic.Value>
+          <Statistic.Value>
+            <CountUp
+                start={0}
+                end={this.props.deaths}
+                duration={1}
+                separator=','
+                delay={0}
+              >
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                  </div>
+                )}
+              </CountUp>
+          </Statistic.Value>
           <Statistic.Label>Deaths {deathPercentage}</Statistic.Label>
         </Statistic>
         <Statistic color='green' className='statSubBox'>
-          <Statistic.Value>{this.numberWithCommas(this.props.recovered)}</Statistic.Value>
+          <Statistic.Value>
+            <CountUp
+              start={0}
+              end={this.props.recovered}
+              duration={0.7}
+              separator=','
+              delay={0}
+              useEasing={true}
+            >
+              {({ countUpRef }) => (
+                <div>
+                  <span ref={countUpRef} />
+                </div>
+              )}
+            </CountUp>
+          </Statistic.Value>
           <Statistic.Label>Recovered {recoverPercentage}</Statistic.Label>
         </Statistic>
       </div>
