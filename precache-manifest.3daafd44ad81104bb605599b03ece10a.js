2c35d0b164f88129a31add75418b99c339a49f5b
Kevin Le <kevin_le@sfu.ca>
2020-05-07 20:56:19 -0700

    Updates

---
+++ precache-manifest.3daafd44ad81104bb605599b03ece10a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed234a9472a6da2f45a909f8eb91b139",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "3551282e03de11025b35",
+    "url": "/covid-19-tracker/static/css/main.e27bdc5a.chunk.css"
+  },
+  {
+    "revision": "159a88b6416d708ba132",
+    "url": "/covid-19-tracker/static/js/2.f854e2cf.chunk.js"
+  },
+  {
+    "revision": "2c90a656d0d44c3faef10d4518d65c63",
+    "url": "/covid-19-tracker/static/js/2.f854e2cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3551282e03de11025b35",
+    "url": "/covid-19-tracker/static/js/main.2bddb80a.chunk.js"
+  },
+  {
+    "revision": "5e06a040ab264cccbd79",
+    "url": "/covid-19-tracker/static/js/runtime-main.ad136df9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19-tracker/precache-manifest.cedd347f1042b1c6a393ff463b5fce5d.js"
+  "/covid-19-tracker/precache-manifest.3daafd44ad81104bb605599b03ece10a.js"
 );
 
 self.addEventListener('message', (event) => {
